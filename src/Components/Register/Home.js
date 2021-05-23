acb44e1421479edc269e4792b7a41bc542a257b1
Vishala Ramasamy <vishala.ramasamy@gmail.com>
2021-05-24 03:34:15 +0530

    company info vh

--- src/Components/Register/Home.js
+++ src/Components/Register/Home.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Home() {
     return (
             <div className="col-md-3 text-center d-flex justify-content-center "
-                  style={{minHeight:'90vh',backgroundColor:'dodgerblue'}}>
+                  style={{minHeight:window.screen.width<=576?'50vh':'90vh',backgroundColor:'dodgerblue'}}>
                     <div class="" >
                         <div class="card-body" >
                             <h3>Company Name</h3>
