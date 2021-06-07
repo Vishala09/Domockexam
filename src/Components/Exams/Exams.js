62a386d5a3a30f658c4beaedaf7daff263da1dd0
Vishala Ramasamy <vishala.ramasamy@gmail.com>
2021-06-07 11:51:21 +0530

    exams icon

--- src/Components/Exams/Exams.js
+++ src/Components/Exams/Exams.js
@@ -170,7 +170,7 @@ function Exams() {
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" 
                     viewBox="0 0 16 16">
   <rect width="4" height="16" x="6" y="1" rx="1"/>
-  <path d="M1.5 14a.5.5 0 0 0 0 1v-1zm13 1a.5.5 0 0 0 0-1v1zm-13 "/>
+  <path d="M1.5 14a.5.5  1a.5.5  "/>
 </svg>
                     </div>}
                  
