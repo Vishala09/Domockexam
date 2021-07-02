0f9397a30b0bcc75f57c282f71bd6071b1262692
Vishala Ramasamy <vishala.ramasamy@gmail.com>
2021-07-02 18:07:51 +0530

    home page - images in timer

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import ExamHall1 from '../../images/ExamHall1.jpeg';
 import ExamHall2 from '../../images/ExamHall2.jpg';
 import ExamHall3 from '../../images/ExamHall3.png'
@@ -6,20 +6,24 @@ import ExamHall4 from '../../images/ExamHall4.jpg'
 import ExamHall5 from '../../images/ExamHall5.jpg';
 import OnlineExam from '../../images/OnlineExam.jpg'
 function Home() {
-    
+    const images = [ExamHall1,ExamHall2,ExamHall4,ExamHall5,OnlineExam];
+    const [Image, setImage] = useState(images[0]);
+    useEffect(() => {
+            let i=0;
+            setInterval(() => {
+                i++;
+                if(i==images.length)
+                {
+                        i=0;
+                }
+                setImage(images[i])
+            }, 2000);
+    }, [])
     return (
         <div style={{top:'20vh'}} className="container-fluid">
             <h4  className="d-flex justify-content-center">Company Logo</h4>
             <div style={{height:'80vh'}}>
-                    <div className="row">
-                        <img className="col-4" height="250px" src={ExamHall1} />
-                        <img className="col-4" height="250px" src={ExamHall4} />
-                        <img className="col-4" height="250px" src={ExamHall2} />
-                </div>
-                <div className="row">
-                        <img className="col-6" height="250px" src={OnlineExam} />
-                        <img className="col-6" height="250px" src={ExamHall5} />
-                    </div>
+                <img height="100%" width="100%" src={Image} />
             </div>
             <div style={{height:'50vh'}}>
                     <h1>About Us</h1>
