1ee7831d70a9123f200536663acf9c2b7f18567f
Vishala Ramasamy <vishala.ramasamy@gmail.com>
2021-07-02 16:36:16 +0530

    question paper page - fill in the blank , home page images - mobile versions

--- src/Components/QuestionPaper/Q2.js
+++ src/Components/QuestionPaper/Q2.js
@@ -3,6 +3,12 @@ import './Qp.css';
 import Questions from './Q2.json'
 function Q2() {
     const [Ques, setQues] = useState(Questions);
+      function dragEnter(event) {
+          event.target.style.border = "3px dotted red";
+      }
+      function dragLeave(event) {
+          event.target.style.border = "";
+      }
       function allowDrop(ev) {
         ev.preventDefault();
       }
@@ -17,20 +23,21 @@ function Q2() {
         ev.preventDefault();
         var data = ev.dataTransfer.getData("text");
         let element = document.getElementById(data);
+        ev.target.style.border = "";
+        Ques[index].match[idx].selectedanswer=element.innerHTML;
+        setQues([...Ques]);
         // let newelement = document.createElement('DIV'); 
         // newelement.innerText=element.innerHTML;
-        if(ev.target.childNodes.length==0)
-        {
-            Ques[index].match[idx].selectedanswer=element.innerHTML;
-            setQues([...Ques]);
-            //ev.target.appendChild(newelement);
-        }
-        else
-        {
-            // let delchild = ev.target.childNodes[0];
-            // ev.target.removeChild(delchild);
-            // ev.target.appendChild(newelement);
-        }
+        // if(ev.target.childNodes.length==0)
+        // {
+        //     ev.target.appendChild(newelement);
+        // }
+        // else
+        // {
+        //      let delchild = ev.target.childNodes[0];
+        //      ev.target.removeChild(delchild);
+        //      ev.target.appendChild(newelement);
+        // }
       }
       function removeSelectedAnswer(index,idx){
         Ques[index].match[idx].selectedanswer="";
@@ -44,7 +51,7 @@ function Q2() {
                         <h4>{index+1}.&nbsp;{el.questionheading} </h4>
                         <div style={{marginLeft:'20px'}}>
                             <h5>{el.question}</h5>
-                            <div style={{width:'100%'}} id={'div'+el.type+index} onDrop={(event)=>drop(event)} onDragOver={(event)=>allowDrop(event)}>
+                            <div style={{width:'100%'}} id={'div'+el.type+index} >
                                 {
                                     el.match.map((m,idx)=>
                                     <> 
@@ -61,8 +68,9 @@ function Q2() {
                                     <div  className="row mb-2">
                                         <div className="col-12 col-md-4">{m.q}</div>
                                         <div className="col-12 col-md-5 d-flex">
-                                            <div className="dropelement col-11" 
-                                            id={'div'+el.type+index} onDrop={(event)=>drop(event,index,idx)} onDragOver={(event)=>allowDrop(event)}>
+                                            <div className="dropelement col-11" onDragEnter={(event)=>dragEnter(event)}
+                                            id={'div'+el.type+index+idx} onDrop={(event)=>drop(event,index,idx)} 
+                                            onDragOver={(event)=>allowDrop(event)} onDragLeave={(event)=>dragLeave(event)} >
                                                     {Ques[index].match[idx].selectedanswer}
                                             </div>
                                             {
