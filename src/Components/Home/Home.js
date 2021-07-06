f3891110f9cf613ae950c1495f5ac1d993668424
Vishala Ramasamy <vishala.ramasamy@gmail.com>
2021-07-06 19:23:32 +0530

    home images

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,12 +1,22 @@
 import React, { useEffect, useState } from 'react'
 import ExamHall1 from '../../images/ExamHall1.jpeg';
 import ExamHall2 from '../../images/ExamHall2.jpg';
-import ExamHall3 from '../../images/ExamHall3.png'
+import ExamHall3p from '../../images/ExamHall3.png'
 import ExamHall4 from '../../images/ExamHall4.jpg'
 import ExamHall5 from '../../images/ExamHall5.jpg';
-import OnlineExam from '../../images/OnlineExam.jpg'
+import OnlineExam1 from '../../images/OnlineExam.jpg';
+
+import OnlineExam2 from '../../images/OnlineExam2.jpg';
+import ExamHall1p from '../../images/ExamHall1.png';
+import ExamHall2p from '../../images/ExamHall2.png';
+import ExamHall6 from '../../images/ExamHall6.jpg';
+import ExamHall7 from '../../images/ExamHall7.jpg';
+
+import OnlineTest1 from '../../images/OnlineTest1.jpg';
+import OnlineTest2 from '../../images/OnlineTest2.jpg';
+import OnlineTest3 from '../../images/OnlineTest3.png';
 function Home() {
-    const images = [ExamHall1,ExamHall2,ExamHall4,ExamHall5,OnlineExam];
+    const images = [ExamHall1p,ExamHall2p, ExamHall6,ExamHall7,OnlineExam2,ExamHall1,ExamHall2,OnlineTest1,OnlineTest2,OnlineTest3];
     const [imageIndex, setimageIndex] = useState(0)
     useEffect(() => {
        // loadImages();
@@ -40,12 +50,12 @@ function Home() {
   }
     return (
         <div style={{top:'20vh'}} className="container-fluid">
-            <h4  className="d-flex justify-content-center">Company Logo </h4>
-            <div  >
-                    <div >
-                            <button onClick={()=>goLeft()} className="btn btn-primary" style={{position:'absolute',top:'50%',left:20,zIndex:1}}><i class="fa fa-chevron-left"></i></button>
-                            <button onClick={()=>goRight()} className="btn btn-primary" style={{position:'absolute',top:'50%',right:20,zIndex:1}}><i class="fa fa-chevron-right"></i></button>
-                            <img height="450px" width="100%" src={images[imageIndex]} />  
+            <h4  className="d-flex justify-content-center ">Company Logo </h4>
+            <div  style={{background:'dodgerblue',width:'100%',display:'flex',justifyContent:'center',alignItems:'center'}}>
+                    <div style={{display:'flex',justifyContent:'center',alignItems:'center'}} >
+                        <button onClick={()=>goLeft()} className="btn btn-primary" style={{position:'absolute',top:'50%',left:20,zIndex:1}}><i class="fa fa-chevron-left"></i></button>
+                        <button onClick={()=>goRight()} className="btn btn-primary" style={{position:'absolute',top:'50%',right:20,zIndex:1}}><i class="fa fa-chevron-right"></i></button>
+                        <img height="450px" width="80%" src={images[imageIndex]} />  
                     </div>
             </div>
             <div style={{position:'relative',marginTop:'50px'}}>
